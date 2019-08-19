1d7be390737e5c7127f336629da1b8d07da4d596
chengaoyuan <chen.gaoyuan@qq.com>
2019-08-19 14:56:07 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
 export * from "./types";
 export * from "./struct";
-
-import { TypeStruct, INT8, INT16, INT32, INT64, UINT16, UINT32, UINT64, UINT8, CHAR, FLOAT, DOUBLE, WORD, DWORD } from "./../src";
-class Test extends TypeStruct {
-    //
-}
